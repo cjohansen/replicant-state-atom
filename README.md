dd06379fc8167935fbb2b18801295bcb534e4dac
Christian Johansen <christian@cjohansen.no>
2024-12-28 19:29:58 +0100

    Add routing to the setup

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ The development build is initialized from [the dev
 namespace](./dev/state_atom/dev.cljs). There is also a [prod
 namespace](./src/state_atom/prod.cljs) which could be used as the target for a
 production build.
+
+There is a router setup here as well. Check out the [state-setup
+branch](https://github.com/cjohansen/replicant-state-atom/tree/state-setup) for
+the version that only has state management.
--- src/state_atom/core.cljs
+++ src/state_atom/core.cljs
@@ -1,7 +1,28 @@
 (ns state-atom.core
   (:require [clojure.walk :as walk]
+            [replicant.alias :as alias]
             [replicant.dom :as r]
+            [state-atom.router :as router]
             [state-atom.ui :as ui]))
+
+(defn routing-anchor [attrs children]
+  (let [routes (-> attrs :replicant/alias-data :routes)]
+    (into [:a (cond-> attrs
+                (:ui/location attrs)
+                (assoc :href (router/location->url routes
+                                                   (:ui/location attrs))))]
+          children)))
+
+(alias/register! :ui/a routing-anchor)
+
+(defn find-target-href [e]
+  (some-> e .-target
+          (.closest "a")
+          (.getAttribute "href")))
+
+(defn get-current-location []
+  (->> js/location.href
+       (router/url->location router/routes)))
 
 (defn interpolate-actions [event actions]
   (walk/postwalk
@@ -18,11 +39,20 @@
       :store/assoc-in (apply swap! store assoc-in args)
       (println "Unknown action" action "with arguments" args))))
 
+(defn route-click [e store routes]
+  (let [href (find-target-href e)]
+    (when-let [location (router/url->location routes href)]
+      (.preventDefault e)
+      (if (router/essentially-same? location (:location @store))
+        (.replaceState js/history nil "" href)
+        (.pushState js/history nil "" href))
+      (swap! store assoc :location location))))
+
 (defn main [store el]
   (add-watch
    store ::render
    (fn [_ _ _ state]
-     (r/render el (ui/render-page state))))
+     (r/render el (ui/render-page state) {:alias-data {:routes router/routes}})))
 
   (r/set-dispatch!
    (fn [event-data actions]
@@ -31,5 +61,15 @@
            (:replicant/dom-event event-data))
           (execute-actions store))))
 
+  (js/document.body.addEventListener
+   "click"
+   #(route-click % store router/routes))
+
+  (js/window.addEventListener
+   "popstate"
+   (fn [_] (swap! store assoc :location (get-current-location))))
+
   ;; Trigger the initial render
-  (swap! store assoc :app/started-at (js/Date.)))
+  (swap! store assoc
+         :app/started-at (js/Date.)
+         :location (get-current-location)))
